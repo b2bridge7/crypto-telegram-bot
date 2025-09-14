8ea077ff3b946581082ce7d9d2b9dc95522063a9
b2bridge7 <askianke@gmail.com>
2025-09-14 19:49:51 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,11 +2,18 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 from config import TELEGRAM_BOT_TOKEN
 from handlers import get_price, start
 
-app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+async def main():
+    
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("price", get_price))
+    
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("price", get_price))
+
+    
+    await app.run_polling()
+
 
 if __name__ == "__main__":
-    print("✅ Бот запущен. Жди команд...")
-    app.run_polling()
+    import asyncio
+    asyncio.run(main())
